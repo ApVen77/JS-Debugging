a8c48565e425dd34dbd3a3d06a2af113c2297ff6
ApVen77 <42076342+ApVen77@users.noreply.github.com>
2024-03-21 17:25:06 -0600

    updated JS file

--- app.js
+++ app.js
@@ -1,65 +1,53 @@
-console.log("JS is Alive!");
-
 //#1: Fix syntax errors first. Run the following code as-is and read the error message. Fix the mistake, and then re-run the code to check it.
-let launchReady = false;
-let fuelLevel = 17000;
+// let launchReady = false;
+// let fuelLevel = 17000;
 
-if (fuelLevel >= 20000) {
-  console.log("Fuel level cleared.");
-  launchReady = true;
-} else {
-  console.log("WARNING: Insufficient fuel!");
-  launchReady = false;
-}
+// if (fuelLevel >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launchReady = false;
+// }
 
 //#2: The next block of code hides two syntax errors. Run the code as-is to find the mistakes.
-let launchReadyOne = false;
-let crewStatus = true;
-let computerStatus = "green";
+// let launchReady = false;
+// let crewStatus = true;
+// let computerStatus = 'green';
 
-if (crewStatus && computerStatus === "green") {
-  console.log("Crew & computer cleared.");
-  launchReadyOne = true;
-} else {
-  console.log("WARNING: Crew or computer not ready!");
-  launchReadyOne = false;
-}
+// if (crewStatus && computerStatus === 'green') {
+//    console.log('Crew & computer cleared.');
+//    launchReady = true;
+// } else {
+//    console.log('WARNING: Crew or computer not ready!');
+//    launchReady = false;
+// }
 
-if (launchReadyOne) {
-  console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
-  console.log("Fed parrot...");
-  console.log("Ignition...");
-  console.log("Liftoff!");
-} else {
-  console.log("Launch scrubbed.");
-}
+// if (launchReady) {
+//    console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+//    console.log("Fed parrot...");
+//    console.log("Ignition...");
+//    console.log("Liftoff!");
+// } else {
+//    console.log("Launch scrubbed.");
+// }
 
-//#4: The next block of code hides four syntax errors. Run the code as-is to find the mistakes.
-var numKids = "5";
-var partner = "David Beckham";
-var geoLocation = "Costa Rica";
-var jobTitle = "web developer";
+//#4: The next block of code hides three reference errors one syntax errors. Run the code as-is to find the mistakes.
+// var numKids  = 5;
+// var partner  = 'David Beckham';
+// var geoLocation = 'Costa Rica';
+// var jobTitle = 'web developer';
 
-var future =
-  "You will be a " +
-  jobTitle +
-  " in " +
-  geoLocation +
-  ", and married to " +
-  partner +
-  " " +
-  " with " +
-  numKids +
-  " kids.";
-console.log(future);
+// var Future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' +
+//    partner + ' ' + ' with ' + numKids + ' kids.';
+// console.log(Future);
 
 //#5: The next block of code hides one logical error. Run the code as-is to find the mistakes.
 var birthYear = 1984;
 var futureYear = 2012;
 var age;
 age = futureYear - birthYear;
-
-console.log(`I will be either  ${age}   or ${age - 1}`);
+console.log(`I will be either  ${age}  or ${age - 1}`);
 
 //#6: The next block of code hides one logical error. Run the code as-is to find the mistakes.
 // Multiplication tables
@@ -73,22 +61,24 @@ for (var i = 0; i <= 10; i++) {
 //#7: The next block of code hides three  logical error. Run the code as-is to find the mistakes.
 //The for loop will iterate from 0 to 20, but should stop at 18 then continue
 
-for (var j = 0; i <= 20; i++) {
-  if (j % 2 === 0) {
-    console.log(j + " is even");
+for (var i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " is even");
   } else {
-    console.log(j + " is odd");
+    console.log(i + " is odd");
   }
 }
-//#8: The next block of code hides one-two  logical error. Run the code as-is to find the mistakes.
-let a = 5;
-let b = 6;
-let c = a + b;
-while (a > b) {
-  console.log(c);
-}
 
-//#9: The next block of code hides two syntax errors. Run the code as-is to find the mistakes.
+//#8: The next block of code hides one   logical error. Run the code as-is to find the mistakes.
+// a = 5;
+// b = 6;
+// c = a + b;
+// while( a < b) {
+// console.log(c);
+// i++
+// }
+
+//#9: The next block of code hides two logical errors. Run the code as-is to find the mistakes.
 let text = "";
 let i = 0;
 do {
